f8010ea4db1e1c1b479393ac94c9d4f607ebe965
Frederik Max Bensimon <fmbe58393@edu.ucl.dk>
2024-10-11 10:08:57 +0200

    Max has added admin site

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurfsupEmil.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GoToSurfboards()
+        {
+            return RedirectToAction("Index", "Surfboards");
+        }
+        public IActionResult GoToOrders()
+        {
+            return RedirectToAction("Index", "Orders");
+        }
+    }
+}
