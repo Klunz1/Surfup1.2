ea7dc04a26484398db3991022fd71ae820af72a3
Klunz1 <Klunz@live.dk>
2024-12-18 09:14:04 +0100

    Hej

--- Data/Migrations/20241023072637_backinbusiness.Designer.cs
+++ Data/Migrations/20241023072637_backinbusiness.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SurfsupEmil.Data;
 
@@ -11,9 +12,11 @@ using SurfsupEmil.Data;
 namespace SurfsupEmil.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241023072637_backinbusiness")]
+    partial class backinbusiness
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241023072523_backinbusiness.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurfsupEmil.Migrations
+{
+    /// <inheritdoc />
+    public partial class backinbusiness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TotalPrice",
+                table: "Orders",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
